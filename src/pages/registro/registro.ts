8f4865394eddc379c31d0d5ffb3510d3900d4654
daniel9898 <danyillusion@yahoo.com.ar>
2018-05-04 17:59:31 -0300

    ...

--- src/pages/registro/registro.ts
+++ src/pages/registro/registro.ts
@@ -3,6 +3,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { User } from '../../clases/usr';
 import { AngularFireAuth } from 'angularfire2/auth'
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 @IonicPage()
 @Component({
@@ -15,18 +18,47 @@ export class RegistroPage {
 
   constructor(public navCtrl: NavController, 
   	          public navParams: NavParams,
-  	          private ofauth: AngularFireAuth) {
+  	          private ofauth: AngularFireAuth,
+              private toastCtrl: ToastController,
+              private alertCtrl: AlertController) {
 
   	this.usr = new User();
   }
+
+  presentAlert(msj : string) {
+    let alert = this.alertCtrl.create({
+      title: 'Informe de Ingreso : ',
+      subTitle: msj,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  presentToast() {
+
+    let toast = this.toastCtrl.create({
+      message: 'Usted se registro exitosamente !!',
+      duration: 3000,
+      position: 'middle'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+}
 
   async registrarse(){
    
   	try{
       const result = await this.ofauth.auth.createUserWithEmailAndPassword(this.usr.email,this.usr.clave);
-      console.log("result : ",result); 
+      console.log("result : ",result);
+      this.presentToast();
+      //redirect 
   	}catch(e){
-  	  console.log("ERROR : ",e);  
+  	  console.log("ERROR : ",e); 
+      this.presentAlert(e.message); 
   	}
 
   }
